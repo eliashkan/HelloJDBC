2b73eb4ff3d8ea5aa6d1191c1e64c5d0c52555aa
Elias Khan <khan.elias@gmail.com>
2020-07-09 12:02:30 +0200

    connection test oké

--- src/main/java/be/vdab/jdbc/ConnectDB.java
+++ src/main/java/be/vdab/jdbc/ConnectDB.java
@@ -1,7 +1,21 @@
 package be.vdab.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectDB {
     public static void main(String[] args) {
+
+        try (Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/beersdb?serverTimezone=UTC",
+                "root",
+                "1Dv1MyjL$f&D*msQkbHJ93ud#dGw%G"
+        )) {
+            System.out.println("connection is good");
+        } catch (Exception e) {
+            System.out.println("connection no good");
+            e.printStackTrace(System.err);
+        }
 
     }
 }
