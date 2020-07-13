e692509be9029470181727e3e62a7834aac4f95a
Elias Khan <khan.elias@gmail.com>
2020-07-13 09:42:06 +0200

    meerdere queries podracht 6

--- src/main/java/be/vdab/jdbc/GetBeerByAlcohol.java
+++ src/main/java/be/vdab/jdbc/GetBeerByAlcohol.java
@@ -17,11 +17,23 @@ public class GetBeerByAlcohol {
             float valueToFillInQuestionMark = 5F;
             prepstat.setFloat(1, valueToFillInQuestionMark);
             ResultSet rs = prepstat.executeQuery();
+            System.out.println("These are all 5° beers:");
             while (rs.next()){
                 System.out.println(rs.getString("Name"));
                 System.out.println(rs.getFloat("Alcohol"));
 
             }
+
+            // new alcohol level query
+            valueToFillInQuestionMark = 10F;
+            prepstat.setFloat(1, valueToFillInQuestionMark);
+            rs = prepstat.executeQuery();
+            System.out.println("These are all 10° beers");
+            while (rs.next()) {
+                System.out.println(rs.getString("Name"));
+                System.out.println(rs.getFloat("Alcohol"));
+            }
+
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
